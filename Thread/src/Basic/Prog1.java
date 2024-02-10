f181f1f4bc35d4df211c29970b3867237a96f49b
PRAVEEN <PRAVEEN@DESKTOP-T94BTI2>
2024-02-10 03:12:22 +0000

    First Program

---
+++ Thread/src/Basic/Prog1.java
@@ -0,0 +1,26 @@
+// either use interface Runnable or class Thread to create thread
+
+package Basic;
+
+//class FirstThread extends Thread  
+
+class FirstThread implements Runnable
+{
+	public void run()
+	{
+		for(int i=0;i<20;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
+public class Prog1 {
+
+	public static void main(String[] args) {
+		
+		FirstThread  ft = new FirstThread();
+		Thread t = new Thread(ft);
+		t.start();
+	}
+
+}
