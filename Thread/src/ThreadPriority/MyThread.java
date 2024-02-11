7e4e1c709eb63dc154d91f016d5cfff157d64aa0
PRAVEEN <PRAVEEN@DESKTOP-T94BTI2>
2024-02-11 03:57:58 +0000

    PriorityBased

---
+++ Thread/src/ThreadPriority/MyThread.java
@@ -0,0 +1,33 @@
+package ThreadPriority;
+
+class MyClass implements Runnable
+{
+	int count=0;
+	
+	public void run() {
+		for(int i=1;i<3000;i++)
+		     count++;
+			
+			System.out.println("Thread completed :"+Thread.currentThread().getName());
+			System.out.println("Thread Priority :"+Thread.currentThread().getPriority());
+			
+	}			
+}
+
+
+public class MyThread {
+
+	public static void main(String[] args) {
+
+       MyClass mc = new MyClass();
+       Thread t1 = new Thread(mc,"One");
+       Thread t2 = new Thread(mc,"Two");
+       
+       t1.setPriority(2);
+       t2.setPriority(Thread.NORM_PRIORITY);
+       t1.start();
+       t2.start();
+
+	}
+
+}
